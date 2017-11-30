6fefb890da1a4a9a722e113b4b969941b42c2a36
Andrew Keller <Keller192@gmail.com>
2017-11-29 22:32:09 -0500

    adds animations to elevators

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+// var elevatorButton = document.querySelector('[data-translate]');
+// var left = document.querySelector('[data-left]');
+// var right = document.querySelector('[data-right]');
+
+// elevatorButton.addEventListener('click', function() {
+//     left.classList.toggle('translate')
+// });
+
+// elevatorButton.addEventListener('click', function() {
+//     right.classList.toggle('translate')
+// });
